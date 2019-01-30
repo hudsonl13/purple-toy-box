a925e6fa9581dd84b6b1553d1262c1634f7dd56f
Hudson Lessa <admin@hudsonlessa.com>
2019-01-30 16:30:23 -0200

    add pages

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine" , "ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+// Set routes
+app.get("/" , function(req , res)
+{
+    res.render("index.ejs");
+});
+
+app.get("/sobre" , function(req , res)
+{
+    res.render("about.ejs");
+});
+
+app.get("/contato" , function(req , res)
+{
+    res.render("contact.ejs");
+});
+
+// Start web server
+app.listen(process.env.PORT || 8080 , function()
+{
+    console.log("Server started.");
+});
