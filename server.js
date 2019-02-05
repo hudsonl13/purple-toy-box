f8626b465a13bf505706ee2ed4092507d2bce1ef
Hudson Lessa <admin@hudsonlessa.com>
2019-02-05 15:05:23 -0200

    fill About and Contact pages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine" , "ejs");
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
@@ -22,7 +23,7 @@ app.get("/contato" , function(req , res)
     res.render("contact.ejs");
 });
 
-// Start web server
+// Start the web server
 app.listen(process.env.PORT || 8080 , function()
 {
     console.log("Server started.");
